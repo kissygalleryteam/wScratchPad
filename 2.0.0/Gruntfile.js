3e618cd9e9401b7ba4a6b8ef3471a2d27f0db03b
弘树 <tiehang.lth@alibaba-inc.com>
2014-08-06 09:52:44 +0800

    sync daily version 2.0.0

--- 2.0.0/Gruntfile.js
+++ 2.0.0/Gruntfile.js
@@ -96,7 +96,7 @@ module.exports = function(grunt) {
                     proxyHosts:['demo'],
                     servlet:'?',
                     separator:',',
-                    charset:'gbk',
+                    charset:'gbk', 
                     filter:{
                         '-min\\.js':'.js'
                     }
@@ -113,7 +113,7 @@ module.exports = function(grunt) {
                         expand: true,
 						cwd:'<%= pkg.version %>/',
                         src: ['**/*.less',
-							'!build/**/*.less',
+							'!build/**/*.less',   
 							'!demo/**/*.less'],
                         dest: '<%= pkg.version %>/build/',
                         ext: '.less.css'
@@ -127,7 +127,7 @@ module.exports = function(grunt) {
         			expand: true,
 					cwd:'<%= pkg.version %>/',
 					src: ['**/*.scss',
-						'!build/**/*.scss',
+						'!build/**/*.scss',   
 						'!demo/**/*.scss'],
 					dest: '<%= pkg.version %>/build/',
         			ext: '.scss.css'
@@ -145,27 +145,12 @@ module.exports = function(grunt) {
 							'**/*.css',
 							'!build/**/*.css',
 							'!demo/**/*.css'
-						],
-						dest: '<%= pkg.version %>/build/',
+						], 
+						dest: '<%= pkg.version %>/build/', 
 						filter: 'isFile'
 					}
 				]
-			},
-            pub : {
-                files: [
-                    {
-                        expand:true,
-                        cwd:'<%= pkg.version %>/',
-                        src: [
-                            'build/**/*',
-                            'demo/**/*',
-                            'guide/**/*'
-                        ],
-                        dest: './',
-                        filter: 'isFile'
-                    }
-                ]
-            }
+			}
 		},
 		// 监听JS、CSS、LESS文件的修改
         watch: {
@@ -225,11 +210,11 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-flexcombo');
     grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-sass');
-
+	
 
 
 	grunt.registerTask('build', '默认构建任务', function() {
-		task.run(['clean:build', 'kmc','uglify', 'copy:main','less','sass','cssmin', 'copy:pub']);
+		task.run(['clean:build', 'kmc','uglify', 'copy','less','sass','cssmin']);
 	});
 
 	// 启动Debug调试时的本地服务：grunt debug
